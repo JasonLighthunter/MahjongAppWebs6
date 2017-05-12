6f2d9eded5fb5e32e1b1fc8095062c602860e460
StevenvdVorm <steven.v.d.vorm@hotmail.com>
2017-05-12 13:08:35 +0200

    finishing display GameList

--- MahjongAppWebs6/src/app/game-list/game-list.component.ts
+++ MahjongAppWebs6/src/app/game-list/game-list.component.ts
@@ -1,79 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Game } from '../models/game';
+import { GameService } from '../services/game.service';
 
-const GAMES: Game[] = [
-{
-  '_id': '5910cc460c6b76001171c548',
-  'createdBy': {
-    '_id': 'bjce.timmermans@student.avans.nl',
-    'name': 'Brian Timmermans'
-  },
-  'createdOn': '2017-05-08T19:51:34.400Z',
-  'gameTemplate': {
-    '_id': 'Snake',
-    'id':  'Snake'
-  },
-  'players': [
-  {
-    '_id': 'bjce.timmermans@student.avans.nl',
-    'name': 'Brian Timmermans',
-  },
-  {
-    '_id': 'wm.aarts@student.avans.nl',
-    'name': 'Wouter Aarts',
-  }
-  ],
-  'maxPlayers': 5,
-  'minPlayers': 1,
-  'state': 'open',
-  'id': '5910cc460c6b76001171c548'
-},
-{
-  '_id': '5910cb8e0c6b76001171c426',
-  'createdBy': {
-    '_id': 'bjce.timmermans@student.avans.nl',
-    'name': 'Brian Timmermans'
-  },
-  'createdOn': '2017-05-08T19:48:30.583Z',
-  'gameTemplate': {
-    '_id': 'Snake',
-    'id': 'Snake'
-  },
-  'players': [
-  {
-    '_id': 'bjce.timmermans@student.avans.nl',
-    'name': 'Brian Timmermans'
-  }
-  ],
-  'maxPlayers': 3,
-  'minPlayers': 2,
-  'state': 'open',
-  'id': '5910cb8e0c6b76001171c426'
-},
-{
-  '_id': '5910caeb0c6b76001171c395',
-  'createdBy': {
-    '_id': 'bjce.timmermans@student.avans.nl',
-    'name': 'Brian Timmermans'
-  },
-  'createdOn': '2017-05-08T19:45:47.238Z',
-  'gameTemplate': {
-    '_id': 'Shanghai',
-    'id': 'Shanghai'
-  },
-  'players': [
-  {
-    '_id': 'bjce.timmermans@student.avans.nl',
-    'name': 'Brian Timmermans'
-  }
-  ],
-  'maxPlayers': 32,
-  'minPlayers': 2,
-  'state': 'open',
-  'id': '5910caeb0c6b76001171c395'
-}
-];
+import { Game } from '../models/game';
 
 @Component({
   selector: 'app-game-list',
@@ -81,10 +10,26 @@ const GAMES: Game[] = [
   styleUrls: ['./game-list.component.scss']
 })
 export class GameListComponent implements OnInit {
-  gameList = GAMES;
+  // gameList = GAMES;
+  gameList: Game[];
+  numberOfGames: number;
 
-  constructor() { }
+  constructor(private gameService: GameService) {}
 
   ngOnInit() {
+    this.getGames();
+  }
+
+  getGames() {
+    this.gameService.games.subscribe(
+      games => {
+        this.gameList = games;
+        if (this.gameList === null) {
+          this.numberOfGames = 0;
+        } else {
+          this.numberOfGames = this.gameList.length;
+        }
+      }
+    );
   }
 }
