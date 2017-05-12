6a29d6c1b614b2428148084f03a79cafc4bf0064
StevenvdVorm <steven.v.d.vorm@hotmail.com>
2017-05-12 15:00:11 +0200

    start of game creation

--- MahjongAppWebs6/src/app/services/game.service.ts
+++ MahjongAppWebs6/src/app/services/game.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Headers } from '@angular/http';
 
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import 'rxjs/add/operator/map';
@@ -7,9 +7,16 @@ import 'rxjs/add/operator/toPromise';
 
 import { Game } from '../models/game';
 
+import { OAUTH_TOKEN, USERNAME } from '../app.config';
+
 @Injectable()
 export class GameService {
   private _url = 'http://mahjongmayhem.herokuapp.com/games';
+
+  private _postHeaders = new Headers({
+    'x-username': USERNAME,
+    'x-token': OAUTH_TOKEN
+  });
 
   public games: BehaviorSubject<Game[]>;
 
@@ -24,4 +31,12 @@ export class GameService {
         this.games.next(response.json());
       }).toPromise();
   }
+
+  // public CreateGame(postGame) {
+  //   return this.http
+  //     .post(this._url, JSON.stringify(postGame), {headers: this._postHeaders})
+  //     .toPromise()
+  //     .then(res => res.json().data as Game)
+  //     .catch(this.handleError);
+  // }
 }
