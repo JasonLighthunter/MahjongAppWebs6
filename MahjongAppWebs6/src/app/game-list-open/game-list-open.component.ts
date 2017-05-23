44d6fb4adb669df6bf5140208924065a34653266
Job Cuppen <jobcuppen@live.nl>
2017-05-23 14:40:45 +0200

    show names of players of open games

--- MahjongAppWebs6/src/app/game-list-open/game-list-open.component.ts
+++ MahjongAppWebs6/src/app/game-list-open/game-list-open.component.ts
@@ -12,30 +12,43 @@ import { Game, GameStateEnum } from '../models/game';
 export class GameListOpenComponent implements OnInit {
   gameList: Game[];
   numberOfGames: number;
-  nameOfGameStateOpen = GameStateEnum[GameStateEnum.open]
+  nameOfGameStateOpen = GameStateEnum[GameStateEnum.open];
 
   constructor(private gameService: GameService) {}
 
   ngOnInit() {
     this.getGames();
   }
-
+  // getGames() {
+  //   this.gameService.games
+  //   .subscribe(games => {
+  //     console.log(games);
+  //     this.gameList = games;
+  //     if (this.gameList === null) {
+  //       this.numberOfGames = 0;
+  //     } else {
+  //       this.numberOfGames = this.gameList.length;
+  //     }
+  //   });
+  // }
   getGames() {
     this.gameService.games
     .subscribe(games => {
       // const nameOfGameStateOpen = GameStateEnum[GameStateEnum.open];
+      if (games !== null) {
+        console.log(games);
+        const openGames = games.filter(game => game.state === this.nameOfGameStateOpen);
+        const openGamesNotFull = openGames.filter(game => game.players.length < game.maxPlayers);
+        this.gameList = openGamesNotFull;
 
-      const openGames = games.filter(game => game.state === this.nameOfGameStateOpen);
-      const openGamesNotFull = openGames.filter(game => game.players.length < game.maxPlayers);
-      this.gameList = openGamesNotFull;
-
-      this.numberOfGames = this.gameList === null ? 0 : this.gameList.length;
+        this.numberOfGames = this.gameList === null ? 0 : this.gameList.length;
+      }
     });
   }
 
   joinGame(gameId) {
-    var game = this.gameList.filter(game => game.id === gameId);
-    if(game.length === 1){
+    const games = this.gameList.filter(game => game.id === gameId);
+    if (games.length === 1) {
       // console.log(this.gameList.filter(game => game.id === gameId)[0]);
       this.gameService.joinGame(gameId) .subscribe(
         game  => console.log(game),
